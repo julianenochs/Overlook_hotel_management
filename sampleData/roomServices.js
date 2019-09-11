012098161732fc16c6690d5562f18e08d826ac89
Julian Enochs <julianenochs@yahoo.com>
2019-09-11 14:51:04 -0600

    Setup fetch and class structure

---
+++ sampleData/roomServices.js
@@ -0,0 +1,25 @@
+const RoomServicesData = {
+    "roomServices": [{
+                "userID": 14,
+                "date": "2019/07/29",
+                "food": "Rustic Concrete Sandwich",
+                "totalCost": 14.9
+            }, {
+                "userID": 100,
+                "date": "2019/10/18",
+                "food": "Rustic Cotton Sandwich",
+                "totalCost": 17.33
+            }, {
+                "userID": 92,
+                "date": "2019/09/25",
+                "food": "Tasty Wooden Sandwich",
+                "totalCost": 11.15
+            }, {
+                "userID": 8,
+                "date": "2019/08/02",
+                "food": "Practical Granite Sandwich",
+                "totalCost": 14.87
+            }
+        ]
+    }
+export default RoomServicesData;
---
+++ sampleData/users.js
@@ -0,0 +1,17 @@
+const UsersData = {
+    users: [{
+                "id": 1,
+                "name": "Matilde Larson"
+            }, {
+                "id": 2,
+                "name": "Chadrick Lowe"
+            }, {
+                "id": 3,
+                "name": "Christian Sporer"
+            }, {
+                "id": 4,
+                "name": "Brook Christiansen"
+            }
+        ]
+    };
+export default UsersData;
