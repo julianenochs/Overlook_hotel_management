edc8391370dcdb0cf99b572f6b980e8948f989ba
Julian Enochs <julianenochs@yahoo.com>
2019-09-11 18:35:45 -0600

    Add functionality to Room class

--- src/Rooms.js
+++ src/Rooms.js
@@ -1,15 +1,33 @@
 class Rooms {
-    constructor(data) {
+    constructor(data, bookingsData) {
         this.data = data;
+        this.bookingsData = bookingsData
 // date
     }
 
-    // getDailyBookings()
-    // getAvailableRoomsToday()
-    // todaysTotalRevenue()
-    // roomsOccupiedToday()
+    getAvailableRoomsToday(date) {
+        let availableBookings = this.bookingsData.bookings.filter(booking => {
+            return booking.date !== date
+        })
+        // console.log(availableBookings)
+        // availableBookings.forEach(room => {
+        // 
+        // })
+    }
+
+    getDailyBookings(date) {
+        let dailyBookings = this.bookingsData.bookings.filter(booking => {
+            return booking.date === date
+        })
+        return dailyBookings
+    }
+
+    todaysTotalRevenue() {
+        
+    }
     // getDateWithMostBookings()
     // getDateWithLeastBookings()
+    // roomsOccupiedToday() ^ I think it's the same functionality as getDailyBookings
 }
 
 export default Rooms;
--- test/Rooms-test.js
+++ test/Rooms-test.js
@@ -1,15 +1,29 @@
 import chai from 'chai';
 import Rooms from '../src/Rooms';
 import RoomsData from '../sampleData/rooms';
+import BookingsData from '../sampleData/bookings'
 const expect = chai.expect;
 
 let rooms
 beforeEach(() => {
-    rooms = new Rooms(RoomsData);
+    // console.log(RoomsData, BookingsData)
+    rooms = new Rooms(RoomsData, BookingsData);
 });
 
 describe('Rooms', () => {
     it('should show the total amount of rooms available for today', () => {
-        
-    })
+        expect(rooms.getAvailableRoomsToday('2019/10/18')).to.eql('')
+    });
+
+    it('should get the bookings for a given date', () => {
+        expect(rooms.getDailyBookings('2019/10/19')).to.eql([{
+            userID: 4,
+            date: '2019/10/19',
+            roomNumber: 5
+        }]);
+    });
+
+    it('should get the total revenue for today', () => {
+        expect(rooms.todaysTotalRevenue()).to.eql('')
+    });
 });
