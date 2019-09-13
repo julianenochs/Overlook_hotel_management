0f3cc06085acec7889beb57a3f09ccf803524dea
Julian Enochs <julianenochs@yahoo.com>
2019-09-13 11:43:00 -0600

    Add get Daily Orders

---
+++ sampleData/inRoomServices.js
@@ -0,0 +1,33 @@
+const inRoomServices = [{
+        service: 'Tickling',
+        costPerHour: 30,
+        availableByHalfHour: true
+    },
+    {
+        service: 'Toe Ropes',
+        costPerHour: 30,
+        availableByHalfHour: true
+    },
+    {
+        service: 'Shibari One Hour',
+        costPerHour: 60,
+        availableByHalfHour: false
+    },
+    {
+        service: 'Flogging',
+        costPerHour: 45,
+        availableByHalfHour: false
+    },
+    {
+        service: 'Private Dance',
+        costPerHour: 85,
+        availableByHalfHour: true
+    },
+    {
+        service: 'Teasing',
+        costPerHour: 45,
+        availableByHalfHour: true
+    }
+]
+
+export default inRoomServices;
